6ed1491bd0ab88decbce052e5e8126d7bed90768
iamserje <iamserje@gmail.com>
2023-06-20 22:22:29 +0300

    fix class .hide

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const orderButton = document.getElementById('order');
 buyButton.addEventListener('click', () => {
    document.querySelector('#main').classList.add('hide');
    document.querySelector('#form').style.display = 'block';
-   document.querySelector('#user_name').value = tg.initDataUnsafe.user.first_name + ' ' + tg.initDataUnsafe.user.last_name;
+   document.querySelector('#user_name').value = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
 });
 
 orderButton.addEventListener('click', () => {
