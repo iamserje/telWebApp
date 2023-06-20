dcb5e398658ba1ac404424613476de4da3ab682a
iamserje <iamserje@gmail.com>
2023-06-20 22:29:44 +0300

    refactor queryselectors to geElementBiId

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const buyButton = document.getElementById('buy');
 const orderButton = document.getElementById('order');
 
 buyButton.addEventListener('click', () => {
-   document.querySelector('#main').classList.add('hide');
-   document.querySelector('#form').style.display = 'block';
-   document.querySelector('#user_name').value = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
+   document.getElementById('main').style.display = 'none';
+   document.getElementById('form').style.display = 'block';
+   document.getElementById('user_name').value = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
 });
 
 orderButton.addEventListener('click', () => {
