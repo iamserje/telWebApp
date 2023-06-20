84b7293596563c57ea275e0c800e3a370de30925
iamserje <iamserje@gmail.com>
2023-06-20 23:56:31 +0300

    hide name value

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const orderButton = document.getElementById('order');
 buyButton.addEventListener('click', () => {
    document.getElementById('main').style.display = 'none';
    document.getElementById('form').style.display = 'block';
-   document.getElementById('user_name').value = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
+   // document.getElementById('user_name').value = tg.initDataUnsafe.user.first_name + ' ' + tg.initDataUnsafe.user.last_name;
 });
 
 orderButton.addEventListener('click', () => {
